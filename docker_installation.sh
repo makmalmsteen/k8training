2bdce49cc41ff33bb7130f37e4106962262b1eed
makmalmsteen <64619453+makmalmsteen@users.noreply.github.com>
2020-06-29 19:15:33 -0500

    Update docker_installation.sh

--- docker_installation.sh
+++ docker_installation.sh
@@ -8,12 +8,25 @@ sudo systemctl enable docker
 
 echo "installing kubernetes for Ubuntu"
 
-sudo apt install apt-transport-https curl
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
-sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
-sudo apt install kubeadm kubelet kubectl kubernetes-cni
-
-"initializing kubernetes admin"
-sudo kubeadm init
+#sudo apt install apt-transport-https curl
+#curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
+#sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
+#sudo apt install kubeadm kubelet kubectl kubernetes-cni
 
 
+#sudo apt-get update
+#sudo apt-get install -y kubelet kubeadm kubectl
+#sudo apt-mark hold kubelet kubeadm kubectl
+
+
+
+
+
+sudo apt-get update && sudo apt-get install -y apt-transport-https curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
