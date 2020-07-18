62da17513cfdad1421fd5858ce4732402e4378c9
makmalmsteen <64619453+makmalmsteen@users.noreply.github.com>
2020-07-17 21:33:53 -0500

    Update master_node.sh

--- master_node.sh
+++ master_node.sh
@@ -32,7 +32,8 @@ sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 echo "adding a network"
-kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/bc79dd1505b0c8681ece4de4c0d86c5cd2643275/Documentation/kube-flannel.yml
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+# "kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/bc79dd1505b0c8681ece4de4c0d86c5cd2643275/Documentation/kube-flannel.yml
 
 echo "**********************"
 echo "Now install kubernetes in the worker nodes"
